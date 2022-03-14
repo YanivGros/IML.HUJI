c34316d64b1f49de3451e504d033333c700d255e
Yaniv <yaniv.grosskopf@mail.huji.ac.il>
2022-03-14 14:12:18 +0200

    keep working

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -1,16 +1,28 @@
+import numpy
+
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+    sample_size_1000 = numpy.random.normal(1, 10, 1000)
+    estimator = UnivariateGaussian(False)
+    estimator.fit(sample_size_1000)
+    print(f"({estimator.mu_},{estimator.var_})")
 
     # Question 2 - Empirically showing sample mean is consistent
+    for i in range(10, 1001, 10):
+        current_estimator = UnivariateGaussian(True)
+        current_estimator.fit(sample_size_1000[0:i])
+
+
     raise NotImplementedError()
+    np.g
 
     # Question 3 - Plotting Empirical PDF of fitted model
     raise NotImplementedError()
